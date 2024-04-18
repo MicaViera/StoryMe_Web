66743b370e2d32b4e8bc7313eb20693fb2ca314e
MicaViera <vieranoelia98@gmail.com>
2024-04-18 12:49:23 -0700

    Images

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
-from flask import Flask, send_from_directory
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return send_from_directory("static" ,"StoryMe_Website.html")
+    return send_file("api/static/StoryMe_Website.html")
